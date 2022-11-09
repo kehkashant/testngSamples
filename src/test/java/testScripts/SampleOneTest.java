39e7eb6ab724fa130ee308506f04a3a129c0865a
Darkhsha Tamboli <55017970+kehkashant@users.noreply.github.com>
2022-11-08 23:31:57 -0800

    Updated by Tester2

--- src/test/java/testScripts/SampleOneTest.java
+++ src/test/java/testScripts/SampleOneTest.java
@@ -6,7 +6,8 @@ public class SampleOneTest {
   @Test(groups="featureOne")
   public void ADOTest() {
 	  System.out.println("ADO ");
-	  System.out.println("Modified for Githu\b Integration...");
+	  System.out.println("Modified for Github Integration...");
+	  System.out.println("Modified by Tester2 ...");
   }
   
   
